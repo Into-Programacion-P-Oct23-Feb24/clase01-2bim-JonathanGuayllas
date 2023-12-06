dcc32aeaeecb09f93984548483ed9438b87dae2d
Jonathan <jdguayllas@utpl.edu.ec>
2023-12-06 16:41:40 -0500

    Ejercicio Resuelto

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -14,13 +14,16 @@ public class Ejemplo07 {
     public static void main(String[] args) {
         int operacion;
         String mensajeFinal = "";
+        // contador que indica el número del tabla
         for (int i = 1; i <= 50; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
             // el \n también podria ir al inicio
             mensajeFinal = String.format("%sTabla de multiplicar del"
                     + " número %d\n",
                     mensajeFinal, i);
+            // se hace la tabla que se requiere
             for (int contador = 1; contador <= 12; contador++) {
+                // operacion = el número que tenga i * el valor del contador
                 operacion = i * contador;
                 // System.out.printf("%d x %d = %d\n", i, contador, operacion);
                 mensajeFinal = String.format("%s%d x %d = %d\n",
--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -5,6 +5,9 @@
  */
 package paquete03;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -13,8 +16,39 @@ public class Ejemplo03 {
     
     public static void main(String[] args) {
         
-        for (int i = 0; i < 10; i++) {
-            
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        String nombre;
+        String posicion;
+        int edad;
+        double estatura;
+        // la cadena acumula listado de jugadores
+        String cadenaA = "Listado de jugadores\n";      
+        
+        System.out.println("Ingrese la cantidad de jugadores a ingresar");
+        int limite = entrada.nextInt();
+        entrada.nextLine();
+        
+        for (int contador = 1; contador <= limite; contador++) {
+            System.out.println("Ingrese el nombre del Jugador: ");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese la posición en el campo: ");
+            posicion = entrada.nextLine();
+            System.out.println("Ingrese la edad del Jugador: ");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del Jugador: ");
+            estatura = entrada.nextDouble();
+            entrada.nextLine();
+            cadenaA = String.format ("%s%d. %s -%s-, edad %d, estatura"
+                    + " %.2f\n",
+                    cadenaA,
+                    contador,
+                    nombre,
+                    posicion,
+                    edad,
+                    estatura); 
         }
+        System.out.printf(cadenaA);
     }
 }
